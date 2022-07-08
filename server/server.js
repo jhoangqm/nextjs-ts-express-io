44c146b95f6c71e41125c1a1e0222e8bf5b3e449
jhoangqm <jimmyhoangmtl@gmail.com>
2022-07-08 11:30:47 -0400

    Fixes backend server for websocket

--- server/server.js
+++ server/server.js
@@ -1,21 +1,33 @@
+// requires
+const cors = require('cors');
+
+// Port used by express server
+const port = process.env.PORT || 5000;
+
+// use cors
+app.use(cors);
+
 // Create express server app
 const express = require('express');
 const app = express();
 
 // Create http server
-const http = require('http');
-const server = http.createServer(app);
+const httpServer = require('http').createServer(app);
+// Web Socket called on the server with cors
+const { ioServer } = require('socket.io');
+const io = new ioServer(httpServer, {
+  //CORS is needed because HTTP requests sent by the frontend are allowed to reach the server.
+  cors: {
+    origin: 'http://localhost:3000', // This will communicate with the client
+  },
+});
 
-// IO websocket creation
-const { Server } = require('socket.io');
-const io = new Server(server);
-
-const port = process.env.PORT || 5000;
-
+// route that is used to send data to a file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// IO websocket connection
 io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('chat message', (msg) => {
@@ -23,6 +35,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(port, () => {
+// Server is listening on that port
+httpServer.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
